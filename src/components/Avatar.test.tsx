a59a6ae3cc0ec9989ad39fafe2c9d8a614770b68
Duane Fields <duane@duanefields.com>
2024-07-24 15:59:42 -0500

    Unit tes for Avatar component

---
+++ src/components/Avatar.test.tsx
@@ -0,0 +1,70 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { render, screen } from "@testing-library/react"
+import { http, HttpResponse } from "msw"
+import { setupServer } from "msw/node"
+import { afterAll, afterEach, beforeAll, expect, it } from "vitest"
+import { BGG_PROXY } from "../lib/api"
+
+import invalidUsername from "../test/users/invalidUsername.json"
+import pandyandy from "../test/users/pandyandy.json"
+import AlexvW from "../test/users/AlexvW.json"
+
+import Avatar from "./Avatar"
+
+const server = setupServer(
+  http.get(`${BGG_PROXY}/user`, ({ request }) => {
+    const url = new URL(request.url)
+    const username = url.searchParams.get("name")
+    switch (username) {
+      case "pandyandy":
+        return HttpResponse.json(pandyandy)
+      case "AlexvW":
+        return HttpResponse.json(AlexvW)
+      default:
+        return HttpResponse.json(invalidUsername)
+    }
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+// wrap the component in a fresh QueryClientProvider to isolate the tests
+const renderWithQueryProvider = (children: React.ReactNode) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  })
+  return render(<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>)
+}
+
+it("Should render the user's avatar image if they have one", async () => {
+  renderWithQueryProvider(<Avatar username="pandyandy" />)
+  const avatarElement = await screen.findByAltText("pandyandy")
+  expect(avatarElement.tagName).toBe("IMG")
+  expect(avatarElement).toHaveAttribute(
+    "src",
+    "https://cf.geekdo-static.com/avatars/avatar_id180271.jpg",
+  )
+  expect(avatarElement).toBeVisible()
+})
+
+it("Should render a default user icon if they don't have an avatar", async () => {
+  renderWithQueryProvider(<Avatar username="AlexvW" />)
+  const avatarElement = await screen.findByTitle("AlexvW")
+  expect(avatarElement.parentElement?.tagName).toBe("svg")
+  expect(avatarElement.parentElement).toHaveAttribute("data-icon", "user")
+  expect(avatarElement.parentElement).toBeVisible()
+})
+
+it("Should render a default user icon for an invalid username", async () => {
+  renderWithQueryProvider(<Avatar username="invalidUsername" />)
+  const avatarElement = await screen.findByTitle("invalidUsername")
+  expect(avatarElement.parentElement?.tagName).toBe("svg")
+  expect(avatarElement.parentElement).toHaveAttribute("data-icon", "user")
+  expect(avatarElement.parentElement).toBeVisible()
+})
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { getUser } from "../lib/api"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 
 interface AvatarProps {
   /** The username of the avatar you want to load */
@@ -12,10 +14,22 @@ const Avatar = ({ username }: AvatarProps) => {
     queryFn: () => getUser(username),
   })
 
+  // if the user doesn't have an avatar or there is an error, render a default icon
+  if ((query.isSuccess && query.data?.avatar === null) || query.isError) {
+    return (
+      <FontAwesomeIcon
+        icon={faUser}
+        width={64}
+        height={64}
+        size="4x"
+        title={username}
+        titleId="1000"
+      />
+    )
+  }
+
   return (
     <div>
-      {query.isLoading && <div>Loading...</div>}
-      {query.isError && <div>Error: {query.error.message}</div>}
       {query.isSuccess && query.data?.avatar && (
         <div>
           <img src={query.data?.avatar} alt={username} width={64} height={64} />
