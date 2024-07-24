cf8f2d972d47dfe6eb7a9a8095871c5844555a92
Duane Fields <duane@duanefields.com>
2024-07-24 11:44:47 -0500

    add simple test for search box

---
+++ src/components/SearchBox.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react"
+import { expect, it, vi } from "vitest"
+import userEvent from "@testing-library/user-event"
+
+import SearchBox from "./SearchBox"
+
+const onChange = vi.fn(() => {})
+
+it("Should match snapshot", () => {
+  const search = render(<SearchBox searchText="test" onChange={onChange} />)
+  expect(search).toMatchSnapshot()
+})
+
+it("Should match display search text", () => {
+  render(<SearchBox searchText="test" onChange={onChange} />)
+  expect(screen.getByDisplayValue("test")).toBeVisible()
+})
+
+it("Should match display placeholder text", () => {
+  render(<SearchBox searchText="" onChange={onChange} />)
+  expect(screen.getByPlaceholderText("Search by name...")).toBeVisible()
+})
+
+it("Should call the onChange handler on text input", async () => {
+  render(<SearchBox searchText="" onChange={onChange} />)
+  await userEvent.type(screen.getByRole("textbox"), "test")
+  expect(onChange).toHaveBeenCalledTimes(4)
+})
