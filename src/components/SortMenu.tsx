59625fb6ab4aeeebdc0c96e469ecd807155ada7f
Duane Fields <duane@duanefields.com>
2024-07-25 16:42:48 -0500

    Update SortMenu component styling and layout

--- src/components/SortMenu.tsx
+++ src/components/SortMenu.tsx
@@ -17,25 +17,23 @@ const SortMenu = ({ onChange, sort }: SortMenuProps) => {
   }
 
   return (
-    <Box width={150}>
-      <FormControl fullWidth>
-        <InputLabel id="sort-label">Sort By</InputLabel>
-        <Select
-          labelId="sort-label"
-          id="sort"
-          value={sortOrder}
-          label="Sort By"
-          onChange={handleChange}
-        >
-          <MenuItem value="name">Name</MenuItem>
-          <MenuItem value="rating">BGG Rating</MenuItem>
-          <MenuItem value="myRating">My Rating</MenuItem>
-          <MenuItem value="plays">Plays</MenuItem>
-          <MenuItem value="rank">BGG Rank</MenuItem>
-          <MenuItem value="random">Random</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <FormControl sx={{ m: 1, minWidth: 130, maxWidth: 130 }} size="small">
+      <InputLabel id="sort-label">Sort By</InputLabel>
+      <Select
+        labelId="sort-label"
+        id="sort"
+        value={sortOrder}
+        label="Sort By"
+        onChange={handleChange}
+      >
+        <MenuItem value="name">Name</MenuItem>
+        <MenuItem value="rating">BGG Rating</MenuItem>
+        <MenuItem value="myRating">My Rating</MenuItem>
+        <MenuItem value="plays">Plays</MenuItem>
+        <MenuItem value="rank">BGG Rank</MenuItem>
+        <MenuItem value="random">Random</MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
