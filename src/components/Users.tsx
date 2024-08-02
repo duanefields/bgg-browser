3c451c3e8fd9fbecb2589c839c1439086e82da5e
Duane Fields <duane@duanefields.com>
2024-08-02 15:00:01 -0500

    enter username to browse collection

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,16 +1,40 @@
-import { Link } from "@tanstack/react-router"
+import { Link, useNavigate } from "@tanstack/react-router"
 import Avatar from "../components/Avatar"
+import { Button, TextField } from "@mui/material"
+import React from "react"
 
 interface UsersProps {
   usernames: string[]
 }
 
 const Users = ({ usernames }: UsersProps) => {
+  const [username, setUsername] = React.useState("")
+  const navigate = useNavigate()
+
+  const handleSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // todo: update the local storage MRU user list, if it is a valid user
+    navigate({ to: "/user/$username", params: { username } })
+  }
+
   return (
     <>
       {usernames.map((username) => (
         <UserRow key={username} username={username} />
       ))}
+
+      <form onSubmit={handleSubmission}>
+        <TextField
+          type="text"
+          value={username}
+          placeholder="BGG Username"
+          variant="standard"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <Button type="submit" variant="contained">
+          Browse Collection
+        </Button>
+      </form>
     </>
   )
 }
