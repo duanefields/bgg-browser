49d9c15864545d65959e463af5fffa280c728cb2
Duane Fields <duane@duanefields.com>
2024-08-06 12:51:22 -0500

    clean up imports

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
-import React, { Suspense } from "react"
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { createRootRoute, Outlet } from "@tanstack/react-router"
+import React, { Suspense } from "react"
 import NavBar from "../components/NavBar"
 
 const TanStackRouterDevtools =
