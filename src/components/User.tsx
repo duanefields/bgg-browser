66c895beb62e41c060f464a1b310faad575be4c7
Duane Fields <duane@duanefields.com>
2024-08-07 16:25:33 -0500

    Add refresh functionality to User component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,3 +1,7 @@
+import { faSync } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { IconButton } from "@mui/material"
+import { useIsFetching, useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
 import useDebounce from "../hooks/useDebounce"
 import { PlayerCount, Playtime, SortOrder } from "../shared.types"
@@ -7,9 +11,6 @@ import PlaytimeMenu from "./PlaytimeMenu"
 import SearchBox from "./SearchBox"
 import SortMenu from "./SortMenu"
 import classes from "./User.module.css"
-import { IconButton } from "@mui/material"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSync } from "@fortawesome/free-solid-svg-icons"
 
 interface UserProps {
   username: string
@@ -20,6 +21,8 @@ const User = ({ username }: UserProps) => {
   const [sort, setSort] = useState<SortOrder>("name")
   const [players, setPlayers] = useState<PlayerCount>(0)
   const [playtime, setPlaytime] = useState<Playtime>(0)
+  const queryClient = useQueryClient()
+  const isFetching = useIsFetching()
 
   const onSearchTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value)
@@ -38,13 +41,17 @@ const User = ({ username }: UserProps) => {
     setPlaytime(value)
   }
 
+  const onRefreshClick = () => {
+    void queryClient.invalidateQueries({ queryKey: ["collection", username] })
+  }
+
   return (
     <>
       <div className={classes.toolbar}>
         <SortMenu sort={sort} onChange={onSortChange} />
         <PlayersMenu players={players} onChange={onPlayerChange} />
         <PlaytimeMenu playtime={playtime} onChange={onPlaytimeChange} />
-        <RefreshButton />
+        <RefreshButton onClick={onRefreshClick} spin={isFetching > 0} />
       </div>
 
       <div className={classes.header}>
@@ -62,10 +69,15 @@ const User = ({ username }: UserProps) => {
   )
 }
 
-const RefreshButton = () => {
+interface RefreshButtonProps {
+  spin: boolean
+  onClick: () => void
+}
+
+const RefreshButton = ({ spin, onClick }: RefreshButtonProps) => {
   return (
-    <IconButton aria-label="refresh">
-      <FontAwesomeIcon icon={faSync} />
+    <IconButton aria-label="refresh" onClick={onClick}>
+      <FontAwesomeIcon icon={faSync} spin={spin} />
     </IconButton>
   )
 }
