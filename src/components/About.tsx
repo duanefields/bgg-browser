f0f85e97554482007f2f0bc0f5416dc48c5d1aec
Duane Fields <duane@duanefields.com>
2024-09-19 15:55:53 -0500

    adding a reload button to the about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,19 @@
-const About = () => {
-  const VERSION = (import.meta.env.VITE_APP_VERSION as string).trim()
+import { Button } from "@mui/material"
 
-  return <div>About this Application ({VERSION})</div>
+const VERSION = (import.meta.env.VITE_APP_VERSION as string).trim()
+
+const About = () => {
+  const reload = () => {
+    window.location.reload()
+  }
+
+  return (
+    <div>
+      <div>About this Application ({VERSION})</div>
+      <div>
+        <Button onClick={reload}>Reload</Button>
+      </div>
+    </div>
+  )
 }
 export default About
