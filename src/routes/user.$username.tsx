9ee1d2c677d031a5d50f1a085c733c934b6fe5af
Duane Fields <duane@duanefields.com>
2025-02-09 16:03:18 -0600

    WIP - query string state

--- src/routes/user.$username.tsx
+++ src/routes/user.$username.tsx
@@ -1,5 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router"
 import User from "../components/User"
+import { PlayerCount, Playtime, SortOrder } from "../shared.types"
+
+type UserSearch = {
+  sort?: SortOrder
+  players?: PlayerCount
+  playtime?: Playtime
+}
 
 const UserRouteComponent = () => {
   const { username } = Route.useParams()
@@ -8,4 +15,11 @@ const UserRouteComponent = () => {
 
 export const Route = createFileRoute("/user/$username")({
   component: UserRouteComponent,
+  validateSearch: (search: Record<string, unknown>): UserSearch => {
+    return {
+      sort: (search.sort as SortOrder) || "name",
+      players: (search.players as PlayerCount) || 0,
+      playtime: (search.playtime as Playtime) || 0,
+    }
+  },
 })
