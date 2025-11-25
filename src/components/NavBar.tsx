3d98077c6cd7e600a0fa121d0a19d472aea7967c
Duane Fields <duane@duanefields.com>
2025-11-25 17:45:03 -0600

    add required logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@tanstack/react-router"
 import classes from "./NavBar.module.css"
+import bggLogo from "../assets/bgg.png"
 
 const NavBar = () => {
   return (
@@ -12,6 +13,11 @@ const NavBar = () => {
         <Link to="/about" activeProps={{ className: classes.active }}>
           About
         </Link>
+      </div>
+      <div>
+        <a href="https://boardgamegeek.com" target="_blank" rel="noreferrer">
+          <img height="50" src={bggLogo} />
+        </a>
       </div>
       <div className={classes.title}>BGG Collection Browser</div>
     </div>
